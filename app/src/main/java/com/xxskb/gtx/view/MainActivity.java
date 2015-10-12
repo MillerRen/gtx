fc81d076aae1c92550c1b9a737aa4017cfce85fe
Miller Ren <ren12345@126.com>
2015-10-12 15:29:08 +0800

    searchable

--- app/src/main/java/com/xxskb/gtx/view/MainActivity.java
+++ app/src/main/java/com/xxskb/gtx/view/MainActivity.java
@@ -2,15 +2,20 @@ package com.xxskb.gtx.view;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AbsListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.xxskb.gtx.R;
 import com.xxskb.gtx.view.BaseActivity;
@@ -32,6 +37,24 @@ public class MainActivity extends BaseActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        handleIntent();
+
+
+    }
+
+    @Override
+    protected void  onNewIntent(Intent intent){
+        setIntent(intent);
+        handleIntent();
+    }
+
+    private void handleIntent(){
+        Intent intent = getIntent();
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.i("query", query);
+        }
     }
 
     @Override
@@ -47,7 +70,14 @@ public class MainActivity extends BaseActivity {
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setIconified(false);
         searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
-        //onSearchRequested();
+        searchView.setSubmitButtonEnabled(true);
+
+        ImageView icon = (ImageView)searchView.findViewById(android.support.v7.appcompat.R.id.search_mag_icon);
+        icon.setAdjustViewBounds(true);
+        icon.setMaxWidth(0);
+        icon.setLayoutParams(new LinearLayout.LayoutParams(AbsListView.LayoutParams.WRAP_CONTENT, AbsListView.LayoutParams.WRAP_CONTENT));
+        icon.setImageDrawable(null);
+
         return true;
     }
 
