cb101fc8dc2e7734870fba68d9059bcde0b04e06
Miller Ren <ren12345@126.com>
2015-10-13 18:16:45 +0800

    suggestion database

--- app/src/main/java/com/xxskb/gtx/db/SuggestionDataBase.java
+++ app/src/main/java/com/xxskb/gtx/db/SuggestionDataBase.java
@@ -2,8 +2,13 @@ package com.xxskb.gtx.db;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.provider.BaseColumns;
+
+import java.util.HashMap;
 
 /**
  * Created by renyufei on 15-10-13.
@@ -16,6 +21,46 @@ public class SuggestionDataBase {
     private final static String DATABASE_NAME = "skb";
     private final static  int DATABASE_VERSION = 1;
     private final static String SUGGESTION_TABLE = "suggestion";
+
+    private final SuggestionDatabaseHelper mDatabaseHelper;
+    private final HashMap<String, String> mColumnMap = buildColumnMap();
+
+    public SuggestionDataBase(Context context){
+        this.mDatabaseHelper = new SuggestionDatabaseHelper(context);
+    }
+
+    private HashMap<String, String> buildColumnMap(){
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put(KEY_WORD, KEY_WORD);
+        map.put(KEY_DEFINTION, KEY_DEFINTION);
+        map.put(BaseColumns._ID, "rowid as " + BaseColumns._ID);
+        map.put(SearchManager.SUGGEST_COLUMN_INTENT_DATA_ID, "rowid as " + SearchManager.SUGGEST_COLUMN_INTENT_DATA_ID);
+        map.put(SearchManager.SUGGEST_COLUMN_SHORTCUT_ID, "rowid as "+SearchManager.SUGGEST_COLUMN_SHORTCUT_ID);
+
+        return map;
+    }
+
+    public Cursor match(String query, String[] columns){
+        return query();
+    }
+
+    private Cursor query(String selection, String[] selectArgs, String[] columns){
+        SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+        builder.setTables(SUGGESTION_TABLE);
+        builder.setProjectionMap(mColumnMap);
+
+        Cursor cursor = builder.query(mDatabaseHelper.getReadableDatabase(), columns, selection, selectArgs, null, null, null);
+
+        if(null == cursor){
+            return null;
+        }
+        else if(!cursor.moveToFirst()){
+            cursor.close();
+            return null;
+        }
+
+        return cursor;
+    }
 
     private static class SuggestionDatabaseHelper extends SQLiteOpenHelper{
 
@@ -38,11 +83,13 @@ public class SuggestionDataBase {
         public void onCreate(SQLiteDatabase db) {
             mDatabase = db;
             mDatabase.execSQL(CREATE_SUGGESTION_TABLE);
+            loadSuggestion();
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+            db.execSQL("DROP TABLE IF EXISTS" + SUGGESTION_TABLE);
+            onCreate(db);
         }
     }
 
