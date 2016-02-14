3a7305127b585a9f6fa4b35c380edbffcf1218f9
MillerRen <MillerRen@github.com>
2016-02-14 23:37:08 +0800

    BaseApplication onCreate

--- app/src/main/java/com/xxskb/gtx/BaseApplication.java
+++ app/src/main/java/com/xxskb/gtx/BaseApplication.java
@@ -6,4 +6,11 @@ import android.app.Application;
  * Created by renyufei on 15-10-3.
  */
 public class BaseApplication extends Application {
+    private static BaseApplication mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.mInstance = this;
+    }
 }
