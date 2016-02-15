d75fa64db7f59b7912b035e5e7aa96eada67aa6f
MillerRen <MillerRen@github.com>
2016-02-15 23:54:40 +0800

    update activity

---
+++ app/src/main/java/com/xxskb/gtx/view/UpdateActivity.java
@@ -0,0 +1,16 @@
+package com.xxskb.gtx.view;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+import com.xxskb.gtx.R;
+
+public class UpdateActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+
+}
