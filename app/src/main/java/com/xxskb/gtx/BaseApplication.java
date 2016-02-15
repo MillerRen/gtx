433a74b40c6c8d0a4a7a5a188be1be455ac77385
MillerRen <MillerRen@github.com>
2016-02-15 21:26:55 +0800

    revert

--- app/src/main/java/com/xxskb/gtx/BaseApplication.java
+++ app/src/main/java/com/xxskb/gtx/BaseApplication.java
@@ -6,11 +6,4 @@ import android.app.Application;
  * Created by renyufei on 15-10-3.
  */
 public class BaseApplication extends Application {
-    private static BaseApplication mInstance;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        this.mInstance = this;
-    }
 }
