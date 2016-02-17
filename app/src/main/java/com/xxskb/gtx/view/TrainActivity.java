bf35543679dd8e4658da43a6301f9a74731b2ae1
MillerRen <MillerRen@github.com>
2016-02-17 15:52:39 +0800

    train activity layout

--- app/src/main/java/com/xxskb/gtx/view/TrainActivity.java
+++ app/src/main/java/com/xxskb/gtx/view/TrainActivity.java
@@ -1,6 +1,7 @@
 package com.xxskb.gtx.view;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
 import com.xxskb.gtx.R;
@@ -8,7 +9,7 @@ import com.xxskb.gtx.R;
 /**
  * Created by renyufei on 15-10-13.
  */
-public class TrainActivity extends Activity{
+public class TrainActivity extends ListActivity {
     @Override
     public void onCreate(Bundle bundle){
         super.onCreate(bundle);
